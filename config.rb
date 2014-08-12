8504d711168a81a0d16d31f7d4232bbdf5721f28
Keegan Mullaney <keegan@kmauthorized.com>
2014-08-12 20:11:40 +0800

    remove duplicate code
    On branch master
    Changes to be committed:
    
    	modified:   config.rb

--- config.rb
+++ config.rb
@@ -65,7 +65,7 @@ end
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# activate :livereload
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -85,8 +85,6 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
-
-activate :livereload
 
 # Build-specific configuration
 configure :build do
